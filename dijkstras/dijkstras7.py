577f95e7e9008da0a65d8e6b1a33d0c7478a915a
Abhijeet Chakravorty <>
2020-11-27 23:53:17 +0530

    Updated code for dijkstras

--- dijkstras/dijkstras7.py
+++ dijkstras/dijkstras7.py
@@ -42,4 +42,75 @@ class Graph:
         def has_edge(self, src_key, dest_key):
                 return self.vertices[src_key].does_point_to(self.vertices[dest_key])
         
-def dijkstras():
+def dijkstras(g, source):
+        unvisited = set(g)
+        distance = dict.fromkeys(g, float('inf'))
+        distance[source] = 0
+
+        while unvisited != set():
+                closest = min(unvisited, key=lambda v:distance[v])
+                unvisited.remove(closest)
+
+                for neighbour in closest.get_neighbour():
+                        if neighbour in unvisited:
+                                new_distance = distance[closest] + closest.get_weight(neighbour)
+                                if distance[neighbour] > new_distance:
+                                        distance[neighbour] = new_distance
+        return distance
+
+g = Graph()
+while True:
+        print("Choose an option: ")
+        print("1. Add vertex")
+        print("2. Add edges")
+        print("3. Shortest")
+        print("4. Display")
+        print("5. presss 5 or any other key to exit")
+
+        option = int(input())
+        if option==1:
+                key = int(input("Please provide vertex: "))
+                if key not in g:
+                        g.add_vertex(key)
+                else:
+                        print("Vertex already exists")
+        elif option==2:
+                src = int(input("Please provide source: "))
+                dest = int(input("Please provide destination: "))
+                weight = int(input("Please provide weight: "))
+                if src not in g:
+                        print("Vertex {} does not exist".format(src))
+                elif dest not in g:
+                        print("Vertex {} does not exist".format(dest))
+                else:
+                        if not g.has_edge(src, dest):
+                                g.set_edge(src, dest, weight)
+                                g.add_edge(dest, src, weight)
+                        else:
+                                print("Edge already exists")
+        elif option==3:
+                key = int(input("Please provide starting vertex: "))
+                source = g.get_vertex(key)
+                distance = dijkstras(g, source)
+                print("Distance from {}:".format(key))
+                for v in distance:
+                        print("Distance to {}: {}".format(v.get_key(), distance[v]))
+                print()
+        elif option==4:
+                print("Vertices: ", end=" ")
+                for v in g:
+                        print(v.get_key(), end=" ")
+                print()
+
+                print("Edges")
+                for v in g:
+                        for dest in v.get_neighbour():
+                                w = v.get_weight(dest)
+                                print("(src={}, dest={}, weight={})".format(v.get_key(), dest.get_key(), w))
+                print()
+        else:
+                print("Quit")
+                sys.exit()
+
+
+

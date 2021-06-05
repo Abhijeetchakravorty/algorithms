6199c4addb63070b6705b7ccde0ab2cfd3aaa4d5
Abhijeet Chkravorty <chakravorty.abhijeet91@gmail.com>
2021-06-05 13:27:47 +0530

    udpated code for in-order script

--- Javascript/trees/pre-order.js
+++ Javascript/trees/pre-order.js
@@ -28,3 +28,12 @@ let root = new Node(1);
 root.right = new Node(2);
 root.right.left = new Node(3);
 console.log(preorderTraversal(root));
+
+
+root = new Node(1);
+root.left = new Node(2);
+console.log(preorderTraversal(root));
+
+root = new Node(1);
+root.right = new Node(2);
+console.log(preorderTraversal(root));

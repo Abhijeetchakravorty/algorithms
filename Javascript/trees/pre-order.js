a9e9a30e80bc3e02c085b7da2a0d5ef376f91990
Abhijeet Chkravorty <chakravorty.abhijeet91@gmail.com>
2021-06-05 12:59:13 +0530

    updated code for pre-order

---
+++ Javascript/trees/pre-order.js
@@ -0,0 +1,30 @@
+// root -> left -> right
+class Node {
+        constructor(val, left, right) {
+                this.val = (val === undefined ? 0: val);
+                this.left = (left === undefined ? null : left);
+                this.right = (right === undefined ? null : right);
+        }
+}
+
+var preorderTraversal = function(root) {
+        let arr = [];
+        let nodeStack = [];
+        let data = null;
+        while(root !== null || nodeStack.length !== 0) {
+                nodeStack.push(root);
+                data = nodeStack.pop();
+                arr.push(data.val);
+                if (data.left !== null) {
+                        root = data.left;
+                } else {
+                        root = data.right;
+                }
+        }
+        return arr;
+}
+
+let root = new Node(1);
+root.right = new Node(2);
+root.right.left = new Node(3);
+console.log(preorderTraversal(root));

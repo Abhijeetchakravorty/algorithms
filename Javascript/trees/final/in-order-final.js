fa1c4a0d8a4c50845963af3100886468be05575b
Abhijeet Chkravorty <chakravorty.abhijeet91@gmail.com>
2021-06-14 18:23:30 +0530

    updated code for practice

---
+++ Javascript/trees/final/in-order-final.js
@@ -0,0 +1,30 @@
+/// LEFT ------> ROOT ----> RIGHT
+class Node {
+        constructor(val, left, right) {
+                this.val = (val === undefined ? null : val);
+                this.left = (left === undefined ? null : left);
+                this.right = (right === undefined ? null : right);
+        }
+}
+
+var inorderTraversal = function(root) {
+        let res = [];
+        let nodeStack = [];
+        let curr = root;
+        while (curr != null || nodeStack.length > 0) {
+            while (curr != null) {
+                nodeStack.push(curr);
+                curr = curr.left;
+            }
+            curr = nodeStack.pop();
+            res.push(curr.val);
+            curr = curr.right;
+        }
+        return res;
+}
+let root = new Node(1);
+root.left = new Node(5);
+root.right = new Node(2);
+root.right.left = new Node(3);
+let finaArr = inorderTraversal(root);
+console.log(finaArr);
